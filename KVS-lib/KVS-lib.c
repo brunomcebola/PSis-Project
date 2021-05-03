4da25d68315bfccf122d9b70cb4af1fd82f0b72d
Rui Abrantes <ruimiguel.abrantes@gmail.com>
2021-05-03 15:12:25 +0100

    added KVS-lib.c thinggys

---
+++ KVS-lib/KVS-lib.c
@@ -0,0 +1,25 @@
+#include "KVS-lib.h"
+
+int establish_connection(char * group_id, char * secret){
+
+}
+
+int put_value(char * key, char * value){
+
+}
+
+int get_value(char * key, char ** value){
+
+}
+
+int delete_value(char * key){
+
+}
+
+int register_callback(char * key, void (*callback_funcation)(char *)){
+
+}
+
+int close_connetion(){
+
+}
