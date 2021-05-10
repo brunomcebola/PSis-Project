c25fbc7958e562f60f6c5e230a1361d4f71d7029
Rui Abrantes <ruimiguel.abrantes@gmail.com>
2021-05-10 23:52:10 +0100

    added professor's .h

--- Hashtable/hashtable-lib.h
+++ Hashtable/hashtable-lib.h
@@ -10,7 +10,7 @@ typedef struct _key_pair{
 
 unsigned int hash(char * key);
 
-key_pair ** create_hash_table()
+key_pair ** create_hash_table();
 void initialize_hash_table(key_pair ** hash_table);
 
 int put_on_hash_table(key_pair ** hash_table, char * key, char * value);
